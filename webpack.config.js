6559c914c2c27bd1d45735aa0cddcce986cd29ca
Laura Hope <ssteamlaura@gmail.com>
2023-04-19 09:23:55 -0700

    Add initial files.

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ module.exports = {
     new CleanWebpackPlugin(),
     new Dotenv(),
     new HtmlWebpackPlugin({
-      title: 'Shape Tracker',
+      title: 'Bike API',
       template: './src/index.html',
       inject: 'body'
     })
